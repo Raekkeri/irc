3be296f3be4fec1d1d55218ebedf4b7c611b7a56
Teemu Husso <teemu.husso@fudeco.com>
2009-01-13 14:39:01 +0200

    temp commit. connecting to quakenet works, if you send USER after the auth-lines

--- irc.py
+++ irc.py
@@ -2,24 +2,26 @@
 import socket
 import re
 
-
+host = 'irc.fi.quakenet.org'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('irc.quakenet.org', 6667))
+s.connect((host, 6667))
 
 s.send('NICK %s\r\n' % 'raeq_')
-s.send('USER %s %s bla :%s\r\n' % ('raeq_', 'irc.quakenet.org', 'teemu'))
+#s.send('USER %s %s bla :%s\r\n' % ('raeq', 'whatnot', 'teemu husso'))
 
 rexps = (
 )
 
 ping = re.compile(r'ping :(?P<data>\w+)', re.IGNORECASE)
+#notice_auth = re.compile(r'notice auth', re.IGNORECASE)
 
 buffer = ''
 while(1):
 	buffer += s.recv(512)
 	li = buffer.split('\r\n')
 	buffer = li.pop()
+	print 'BUFFER: %s<' % buffer
 
 	for line in li:
 		match = ping.match(line)
@@ -27,5 +29,6 @@ while(1):
 			sendbuf = 'PONG :%s' % match.group('data')
 			s.send(sendbuf)
 			print 'SENT: %s' % sendbuf
-		else:
-			print 'NOT HANDLED: %s' % line
+			continue
+
+		print 'NOT HANDLED: %s' % line
