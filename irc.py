55b363e753772107baef1472fe324c15a12b01a6
unknown <raeq@.(none)>
2009-01-20 21:29:22 +0200

    Added regexps.txt file which contains some regexp lines. Some functionality++

--- irc.py
+++ irc.py
@@ -25,6 +25,31 @@ class Keyboard(threading.Thread):
 
 Keyboard().start()
 
+class RegExpFormat(object):
+	def __init__(self, regexp, format=None):
+		if format == None:
+			format = 'NO FORMAT'
+		self.regexp = regexp
+		self.format = format
+
+	def match(self, line):
+		match = self.regexp.match(line)
+		return match
+
+	def get_format(self):
+		return self.format
+
+
+class RegExpPing(RegExpFormat):
+	def __init__(self, regexp, format):
+		RegExpFormat.__init__(self, regexp, format)
+
+	def match(self, line):
+		#match = RegExpFormat.match(line)
+		#return match
+		# What to do here!!!!!
+		pass
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, 6667))
@@ -32,7 +57,8 @@ s.connect((host, 6667))
 s.send('NICK %s\r\n' % 'raeq_')
 s.send('USER %s %s bla :%s\r\n' % ('raeq', '0', 'teemu husso'))
 
-regexps = helpers.parse_regexps_file('regexps.txt', '!<>!')
+regexps = [RegExpFormat(r[0], r[1]) for r in \
+	helpers.parse_regexps_file('regexps.txt', '!<>!')]
 
 ping = re.compile(r'^ping (?P<data>[:aA-zZ0-9]+)', re.IGNORECASE)
 #numeric_response = re.compile(':[aA-zZ\.:]+ (?P<code>[0-9]+)')
@@ -47,9 +73,12 @@ while(running):
 	for line in li:
 		match_found = False
 		for r in regexps:
-			match = r[0].match(line)
+			match = r.match(line)
 			if match:
-				print r[1] % match.groupdict()
+				if match_found:
+					print "DOUBLE MATCH: %s" \
+						% r.regexp.pattern
+				print r.get_format() % match.groupdict()
 				match_found = True
 		if not match_found:
 			match = ping.match(line)
