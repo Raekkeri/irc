79784480fd26075b807e67927390b7bcb2360382
unknown <user@.otek.ad.oamk.fi>
2009-01-27 11:41:32 +0200

    Added ircregexp.py, to which RegExp-classes were moved.

---
+++ ircregexp.py
@@ -0,0 +1,28 @@
+MESSAGE = 10
+RESPONSE = 99
+
+class RegExpFormat(object):
+	def __init__(self, regexp, format=None):
+		if format == None:
+			format = 'NO FORMAT'
+		self.regexp = regexp
+		self.format = format
+
+	def get_match(self, line):
+		return self.regexp.match(line)
+
+	def match(self, line):
+		match = self.get_match(line)
+		return match, MESSAGE
+
+	def get_format(self):
+		return self.format
+
+
+class RegExpPing(RegExpFormat):
+	def __init__(self, regexp, format):
+		RegExpFormat.__init__(self, regexp, format)
+
+	def match(self, line):
+		match = RegExpFormat.get_match(self, line)
+		return match, RESPONSE
